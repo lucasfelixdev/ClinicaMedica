b3548a0316e5daf3609e3f286eb24db1644a68c7
Lukas Félix <lucas2009jesus@gmail.com>
2023-09-06 22:22:33 -0300

    UPDATE

--- src/main/java/medclinic/api/models/medico/MedicoRepository.java
+++ src/main/java/medclinic/api/models/medico/MedicoRepository.java
@@ -4,5 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 // 1- SPRING DATA FICARÁ AQUI
 // 2- A interface herdará os métodos do JPA passando como parâmetro a Entidade e o tipo do atributo da primary key da Entidade
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-}
+public interface MedicoRepository extends JpaRepository<Medico, Long>{}
